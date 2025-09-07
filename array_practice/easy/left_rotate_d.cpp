770bd39d9bf6f38f16c7d4a4783375178cca85c3
Piyush-rana04 <piyushrana4611@gmail.com>
2025-09-07 21:07:58 +0000

    done

---
+++ array_practice/easy/left_rotate_d.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int temp=0;
+int place=2;
+vector<int> rotate(vector<int>&arr,int size){
+    for(int i=0;i<place;i++){
+        temp=arr[0];
+        arr.erase(arr.begin());
+        arr.push_back(temp);
+    }return arr;
+}
+
+void print(vector<int>arr){
+    for(auto it:arr){
+        cout<<it;
+    }
+}
+int main(){
+    vector<int>arr={1,2,3,4,5,6,7,8,9};
+    int size=9;
+    rotate(arr,size);
+    print(arr);
+}
